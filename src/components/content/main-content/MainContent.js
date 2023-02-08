ecd7d181e2d60adbc6ff2c7442a32e3a3c4de298
aaronUnosquare <aaron.sosa@unosquare.com>
2023-02-07 23:47:47 -0600

    Add API Service and Request - Redux integration

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,51 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import './MainContent.scss';
 import SlideShow from '../slide-show/SlideShow';
 import Paginate from '../paginate/Paginate';
 import Grid from '../grid/Grid';
+import { IMAGE_URL } from '../../../services/movie.service';
+import { getMovies } from '../../../redux/actions/movies';
 
-const MainContent = () => {
-  const [currentPage, setCurrentPage] = useState(1);
+const MainContent = ({ list, movieType, totalPages, page, getMovies }) => {
+  const [currentPage, setCurrentPage] = useState(page);
+  const [images, setImages] = useState([]);
 
-  const images = [
-    {
-      url: 'https://images.pexels.com/photos/436413/pexels-photo-436413.jpeg?auto=compress&cs=tinysrgb&w=500',
-      rating: 7.5,
-    },
-    {
-      url: 'https://images.pexels.com/photos/3417667/pexels-photo-3417667.jpeg?auto=compress&cs=tinysrgb&w=500',
-      rating: 8.5,
-    },
-    {
-      url: 'https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg?auto=compress&cs=tinysrgb&w=500',
-      rating: 7.8,
-    },
-    {
-      url: 'https://images.pexels.com/photos/410734/pexels-photo-410734.jpeg?auto=compress&cs=tinysrgb&w=500',
-      rating: 9.7,
-    },
-    {
-      url: 'https://images.pexels.com/photos/436413/pexels-photo-436413.jpeg?auto=compress&cs=tinysrgb&w=500',
-      rating: 6.5,
-    },
-    {
-      url: 'https://images.pexels.com/photos/3417667/pexels-photo-3417667.jpeg?auto=compress&cs=tinysrgb&w=500',
-      rating: 8.5,
-    },
-    {
-      url: 'https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg?auto=compress&cs=tinysrgb&w=500',
-      rating: 5.5,
-    },
-    {
-      url: 'https://images.pexels.com/photos/410734/pexels-photo-410734.jpeg?auto=compress&cs=tinysrgb&w=500',
-      rating: 9,
-    },
-  ];
+  const HEADER_TYPE = {
+    now_playing: 'Now Playing',
+    popular: 'Popular',
+    top_rated: 'Top Rated',
+    upcoming: 'Upcoming',
+  };
+
+  const randomMovies = list
+    .sort(() => Math.random() - Math.random())
+    .slice(0, 4);
+
+  useEffect(() => {
+    if (randomMovies.length) {
+      const IMAGES = randomMovies.map((image, index) => ({
+        id: index + 1,
+        url: `${IMAGE_URL}${image.backdrop_path}`,
+      }));
+
+      setImages(IMAGES);
+    }
+  }, []);
+
+  useEffect(() => {
+    setCurrentPage(page);
+  }, [page, totalPages]);
 
   const paginate = (type) => {
     if (type === 'prev' && currentPage > 1) {
+      getMovies(movieType, currentPage - 1);
       setCurrentPage((prev) => prev - 1);
     } else {
+      getMovies(movieType, currentPage + 1);
       setCurrentPage((prev) => prev + 1);
     }
   };
@@ -55,19 +53,34 @@ const MainContent = () => {
       {/* Display slideshow */}
       <SlideShow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
-        <div className="movieType">Now playing</div>
+        <div className="movieType">{HEADER_TYPE[movieType]}</div>
         <div className="paginate">
           <Paginate
             currentPage={currentPage}
-            totalPages={10}
+            totalPages={totalPages}
             paginate={paginate}
           />
         </div>
       </div>
       {/* Display grid component */}
-      <Grid images={images} />
+      <Grid />
     </div>
   );
 };
 
-export default MainContent;
+MainContent.propTypes = {
+  list: PropTypes.array.isRequired,
+  movieType: PropTypes.string.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  page: PropTypes.number.isRequired,
+  getMovies: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+  movieType: state.movies.movieType,
+  totalPages: state.movies.totalPages,
+  page: state.movies.page,
+});
+
+export default connect(mapStateToProps, { getMovies })(MainContent);
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,12 +1,84 @@
+import { useState, useEffect, useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import MainContent from '../content/main-content/MainContent';
+import Spinner from '../spinner/Spinner';
+import {
+  loadMoreMovies,
+  setResponsePageNumber,
+} from '../../redux/actions/movies';
 import './Main.scss';
 
-const Main = () => {
+const Main = ({
+  list,
+  loadMoreMovies,
+  page,
+  totalPages,
+  setResponsePageNumber,
+  movieType,
+}) => {
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(page);
+  const mainRef = useRef();
+  const bottomLineRef = useRef();
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    if (currentPage > 1) {
+      console.log('nuevo fetch', currentPage);
+      setResponsePageNumber(currentPage, totalPages);
+      loadMoreMovies(movieType, currentPage);
+    }
+  }, [currentPage]);
+
+  const fetchData = () => {
+    if (page < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+  const handleScroll = () => {
+    const containerHeight = mainRef.current.getBoundingClientRect().height;
+    const { top: bottomLineTop } =
+      bottomLineRef.current.getBoundingClientRect();
+
+    if (bottomLineTop <= containerHeight) {
+      // fetch data
+      fetchData();
+    }
+  };
+
   return (
-    <div className="main">
-      <MainContent />
+    <div className="main" ref={mainRef} onScroll={() => handleScroll()}>
+      {loading ? <Spinner /> : <MainContent />}
+      <div ref={bottomLineRef}></div>
     </div>
   );
 };
 
-export default Main;
+Main.propTypes = {
+  list: PropTypes.array.isRequired,
+  loadMoreMovies: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  setResponsePageNumber: PropTypes.func.isRequired,
+  movieType: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+  page: state.movies.page,
+  totalPages: state.movies.totalPages,
+  movieType: state.movies.movieType,
+});
+
+export default connect(mapStateToProps, {
+  loadMoreMovies,
+  setResponsePageNumber,
+})(Main);
